2ef6d806d8bbadf475baf70c37e7f32eea31b66a
inksp0ts09 <maryanne.mcglone09@gmail.com>
2021-12-06 09:40:14 -0500

    fixed some errors

--- src/app.js
+++ src/app.js
@@ -29,10 +29,11 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
-  descriptionElement = response.data.weather[0].description;
-  humidityElement = response.data.main.humidity;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
