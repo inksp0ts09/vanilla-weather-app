12b1a361b655c65c23e36d0e66ad8e07a2fde463
inksp0ts09 <maryanne.mcglone09@gmail.com>
2021-12-06 09:34:36 -0500

    added the Search Engine

--- src/app.js
+++ src/app.js
@@ -42,7 +42,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "3318919d4a7d1eadf877215ef4b16af7";
-let apiUrl = `htpps://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "3318919d4a7d1eadf877215ef4b16af7";
+  let apiUrl = `htpps://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
